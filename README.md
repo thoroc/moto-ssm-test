9600e7a2cdb771edd0b1c7e6f56c77bc31682c3a
Thomas Roche <thomas.a.roche@gmail.com>
2022-10-05 03:31:18 +0100

    clean up conftest

--- README.md
+++ README.md
@@ -2,5 +2,7 @@ moto 4.0.6
 
 Solution to test ssm get_parameter when calling AWS own params:
 
-    - /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-ebs
-    - /aws/service/ecs/optimized-ami/amazon-linux-2/recommended
+    - /aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-ebs (already present in moto)
+    - /aws/service/ecs/optimized-ami/amazon-linux-2/recommended (not yet in moto)
+
+See the test setup in /tests/conftest.py
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-from unittest import mock
 
 import boto3
 import pytest
@@ -21,43 +20,6 @@ def aws_credentials():
 def mock_ssm_client(aws_credentials):
     with mock_ssm():
         client = boto3.client("ssm", region_name=AWS_REGION)
-
-        # already present in moto
-        # client.put_parameter(
-        #     Name='/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-ebs',
-        #     Type='String',
-        #     Value='ami-stdparam'
-        # )
-
-        # What the lambda requires
-        # client.put_parameter(
-        #     Name='/aws/service/ecs/optimized-ami/amazon-linux-2/recommended',
-        #     Type='String',
-        #     Value='{"image_id": "ami-ecsparam"}'
-        # )
-
-        # def side_effect(path):
-        #     if path == "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended":
-        #         return_value = {
-        #             "Parameter": {
-        #                 "Name": "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended",
-        #                 "Type": "String",
-        #                 "Value": "{\"ecs_agent_version\":\"1.63.1\",\"ecs_runtime_version\":\"Docker version 20.10.13\",\"image_id\":\"ami-002e2fef4b94f8fd0\",\"image_name\":\"amzn2-ami-ecs-hvm-2.0.20220921-x86_64-ebs\",\"image_version\":\"2.0.20220921\",\"os\":\"Amazon Linux 2\",\"schema_version\":1,\"source_image_name\":\"amzn2-ami-minimal-hvm-2.0.20220912.1-x86_64-ebs\"}",
-        #                 "Version": 94,
-        #                 "LastModifiedDate": 1664230158.399,
-        #                 "ARN": "arn:aws:ssm:eu-west-1::parameter/aws/service/ecs/optimized-ami/amazon-linux-2/recommended",
-        #                 "DataType": "text"
-        #             }
-        #         }
-
-        #         return return_value
-        #     else:
-        #         return client.get_parameter(path)
-
-        # client.get_parameter = mock.patch(
-        #     'boto3.client.get_parameter',
-        #     side_effect=side_effect
-        # )
 
         from moto.ssm.models import ssm_backends, Parameter
         ssm_backends["123456789012"]["eu-west-1"]._parameters["/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"].append(Parameter(
